6267e860c5a3d7d7f37a0abe382934cea3e9dbaf
Luke Pickering <luketpickering@googlemail.com>
2018-06-21 23:43:35 -0500

    no longer calls update on external project linedoc on re-build

--- cmake/linedocSetup.cmake
+++ cmake/linedocSetup.cmake
@@ -5,6 +5,7 @@ ExternalProject_Add(linedoc
   PREFIX "${PROJECT_BINARY_DIR}/linedoc"
   GIT_REPOSITORY https://github.com/luketpickering/linedoc.git
   GIT_TAG stable
+  UPDATE_DISCONNECTED 1
   CMAKE_ARGS
   -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
   -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
