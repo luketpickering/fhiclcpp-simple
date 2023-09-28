6fab802087ebb3a4d4f1c4a8921d1e7fa6e701b8
Luke Pickering <luke.pickering@stfc.ac.uk>
2023-09-28 15:27:42 +0100

    adds -c compact option to fhicl-dump

--- fhiclcpp/fhicl-dump.cxx
+++ fhiclcpp/fhicl-dump.cxx
@@ -3,13 +3,23 @@
 
 #include <iostream>
 
+bool compact = false;
+
 int main(int argc, char const *argv[]) {
-  if (argc != 2) {
-    std::cout << "[ERROR]: Expected to be passed a single fcl file name."
+  if ((argc != 2) && (argc != 3)) {
+    std::cout << "[ERROR]: Expected to be passed an option -c compact "
+                 "specifier and a single fcl file name."
               << std::endl;
     return 1;
   }
 
-  fhicl::ParameterSet ps = fhicl::make_ParameterSet(argv[1]);
-  std::cout << ps.to_indented_string() << std::endl;
+  std::string fname = argv[argc - 1];
+  if ((argc == 3) && (std::string(argv[1]) == "-c")) {
+    compact = true;
+  }
+
+  fhicl::ParameterSet ps = fhicl::make_ParameterSet(fname);
+
+  std::cout << (compact ? ps.to_string() : ps.to_indented_string(2))
+            << std::endl;
 }
